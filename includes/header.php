ad4b45ecf7904d47a9b668e38d0dddac9f90a624
jithu2111 <pchanna1@student.gsu.edu>
2026-04-16 14:27:56 +0530

    Leadership button nav bar

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,8 @@ $body_class = $body_class ?? '';
     <div class="header-meta">
         <span class="meta-line">14 DAYS UNTIL ECLIPSE</span>
         <?php if (isset($_SESSION['user'])): ?>
-            <a href="logout.php" class="header-logout">&#8617; Logout</a>
+            <a href="leaderboard.php" class="header-nav-link">&#9733; Hall of Legends</a>
+            <a href="logout.php" class="header-nav-link">&#8617; Logout</a>
         <?php endif; ?>
     </div>
 </header>
